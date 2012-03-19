58fe4e8e472b66fc6267830ac05f33621f602c8b
Raman Gupta <raman@vivosys.com>
2012-03-19 00:05:04 -0400

    If EHLO fails, transition to HELO
    If the EHLO command fails, we check if we depend on pipelining, starttls,
    or auth. If any are required, then immediately throw an Exception, since
    extended SMTP is required for all.
    
    If none are required, then try sending a HELO command for non-extended
    SMTP.

---
+++ src/main/java/me/normanmaurer/niosmtp/delivery/chain/HeloResponseListener.java
@@ -0,0 +1,79 @@
+/**
+* Licensed to niosmtp developers ('niosmtp') under one or more
+* contributor license agreements. See the NOTICE file distributed with
+* this work for additional information regarding copyright ownership.
+* niosmtp licenses this file to You under the Apache License, Version 2.0
+* (the "License"); you may not use this file except in compliance with
+* the License. You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package me.normanmaurer.niosmtp.delivery.chain;
+
+import me.normanmaurer.niosmtp.SMTPException;
+import me.normanmaurer.niosmtp.SMTPResponse;
+import me.normanmaurer.niosmtp.core.SMTPRequestImpl;
+import me.normanmaurer.niosmtp.delivery.Authentication;
+import me.normanmaurer.niosmtp.delivery.SMTPDeliveryAgentConfig;
+import me.normanmaurer.niosmtp.delivery.SMTPDeliveryEnvelope;
+import me.normanmaurer.niosmtp.transport.SMTPClientConstants;
+import me.normanmaurer.niosmtp.transport.SMTPClientSession;
+
+/**
+ * {@link me.normanmaurer.niosmtp.delivery.chain.ChainedSMTPClientFutureListener} implementation which will handle the <code>HELO</code> {@link me.normanmaurer.niosmtp.SMTPResponse}
+ *
+ * It will write the next {@link me.normanmaurer.niosmtp.SMTPRequest} to the {@link me.normanmaurer.niosmtp.transport.SMTPClientSession} with the right {@link me.normanmaurer.niosmtp.SMTPClientFutureListener} added.
+ *
+ * @author Raman Gupta
+ *
+ */
+public class HeloResponseListener extends ChainedSMTPClientFutureListener<SMTPResponse> implements SMTPClientConstants{
+
+
+    /**
+     * Get instance of this {@link me.normanmaurer.niosmtp.delivery.chain.HeloResponseListener} implemenation
+     */
+    public static final HeloResponseListener INSTANCE = new HeloResponseListener();
+
+    private HeloResponseListener() {
+
+    }
+
+    @Override
+    public void onResult(SMTPClientSession session, SMTPResponse response) throws SMTPException {
+        int code = response.getCode();
+
+        String mail = ((SMTPDeliveryEnvelope)session.getAttribute(CURRENT_SMTP_TRANSACTION_KEY)).getSender();
+
+        if (code < 400) {
+
+            Authentication auth = ((SMTPDeliveryAgentConfig)session.getConfig()).getAuthentication();
+            if (auth == null) {
+                next(session, SMTPRequestImpl.mail(mail));
+            } else {
+                switch (auth.getMode()) {
+                case Plain:
+                    next(session, SMTPRequestImpl.authPlain());
+
+                    break;
+                case Login:
+                    next(session, SMTPRequestImpl.authLogin());
+                default:
+                    break;
+                }
+            }
+
+        } else {
+            setDeliveryStatusForAll(session, response);
+
+        }
+
+    }
+
+}
